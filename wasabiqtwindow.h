4f628f2d43f545e3d21572f9514b1fe5c62e3ff5
Christian Becker-Asano <christian@becker-asano.de>
2014-03-19 11:37:00 +0100

    set the CURRENT_VERSION to match the tag 0.7a of this branch

--- wasabiqtwindow.h
+++ wasabiqtwindow.h
@@ -24,7 +24,7 @@
 ********************************************************************************/
 #ifndef WASABIQTWINDOW_H
 #define WASABIQTWINDOW_H
-#define CURRENT_VERSION "0.7.3"
+#define CURRENT_VERSION "0.7a"
 
 #include <QMainWindow>
 #include "WASABIEngine.h"
